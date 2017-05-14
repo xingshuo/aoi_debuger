978a9e2d76e50d91d33cd82403e0e50b2912bcc2
fuqiang <leavethewindoff@163.com>
2017-05-14 15:55:01 -0700

    屏蔽socket相关,以一种极其残暴的方式实现了地图放缩功能

--- init.py
+++ init.py
@@ -9,6 +9,8 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 from sys import exit
 from thread import *
+
+
 os.system("ln -s -f ~/BnHServer/build/block_map/ ./")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,13 +33,18 @@ SCALE_X = None
 SCALE_Z = None
 MAX_ROW = None
 MAX_COL = None
+
+SCALE_X = 108
+SCALE_Z = 173
+MAX_ROW = 32
+MAX_COL = 32
+
 BLK_MAX_ROW = None
 BLK_MAX_COL = None
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT), 0, 32)
-screen.fill(COLOR_CHOCOLATE)
 
 my_font = pygame.font.SysFont("arial", 16)
 chinese_font = pygame.font.Font("SIMSUN.TTC", 10)
@@ -84,15 +91,15 @@ def DelObject(id):
     if ObjectList.get(id, None):
         del ObjectList[id]
 
-conn, addr = s.accept()
-addr = 'Connected with ' + addr[0] + ':' + str(addr[1])
-print addr
+# conn, addr = s.accept()
+# addr = 'Connected with ' + addr[0] + ':' + str(addr[1])
+# print addr
 
 PKG_HEAD_LEN = 3
 BIN_RECV_LEN = 512
 MAP_INIT = False
 
-pygame.display.set_caption("Oh Aoi! "+addr)
+pygame.display.set_caption("Oh Aoi! ")
 
 ReadBuffer = ""
 BlkDict = {}
@@ -103,6 +110,12 @@ def trans_x(x):
 def trans_z(z):
     return SCREEN_HEIGHT - int(z)/SCALE_Z
 
+def trans_screen_x(x):
+    return int(x)
+
+def trans_screen_z(z):
+    return int(z)
+
 def load_map(path):
     global BLK_MAX_ROW,BLK_MAX_COL
     f = open(path, "rb")
@@ -112,42 +125,35 @@ def load_map(path):
     cz = float(f.readline())
     scale = int(f.readline())
     dct = {}
-    debug = {}
     for i in xrange(BLK_MAX_ROW):
         for j in xrange(BLK_MAX_COL):
             ch = ord(f.read(1))
-            if ch == 0:
+            if ch != 0:
                 if not dct.get(i, None):
                     dct[i] = []
                 dct[i].append(j)
-            if not debug.get(ch):
-                debug[ch] = 0
-            debug[ch] = debug[ch] + 1
-            if not BlkDict.get(i):
-                BlkDict[i] = {}
-            BlkDict[i][j] = ch
-    print "debug",debug
 
     f.close()
-    # for r,c in dct.items():
-    #     c.sort()
-    #     start = None
-    #     end = None
-    #     lst = []
-    #     for i,v in enumerate(c):
-    #         if i == 0:
-    #             start = v
-    #             end = v
-    #             continue
-    #         if v-end > 1:
-    #             lst.append((start,end))
-    #             start = v
-    #             end = v
-    #         else:
-    #             end = v
-    #     if start:
-    #         lst.append((start,end))
-    #     BlkDict[r] = lst
+    for r,c in dct.items():
+        c.sort()
+        start = None
+        end = None
+        lst = []
+        for i,v in enumerate(c):
+            if i == 0:
+                start = v
+                end = v
+                continue
+            if v-end > 1:
+                lst.append((start,end))
+                start = v
+                end = v
+            else:
+                end = v
+        if start != None:
+            lst.append((start,end))
+        BlkDict[r] = lst
+
     print "----load map end-----",BLK_MAX_ROW,BLK_MAX_COL
 
 def handle_socket(conn):
@@ -201,47 +207,94 @@ def handle_socket(conn):
         ReadBuffer = ReadBuffer[cur_idx:]
     print "handle socket end!!!!!"
 
-start_new_thread(handle_socket, (conn,))
+# start_new_thread(handle_socket, (conn,))
+
+load_map("block_map/jingzhou.bytes")
+
+g_Clock = pygame.time.Clock()
+
+g_MouseClickList = []
+
+g_lt_row = 0
+g_lt_col = 0
+g_rb_row = MAX_ROW - 1
+g_rb_col = MAX_COL - 1
 
 while True:
+    global g_lt_row,g_lt_col,g_rb_row,g_rb_col
     for event in pygame.event.get():
         if event.type == QUIT:
             exit()
+        elif event.type == MOUSEBUTTONDOWN:
+            if event.button == 1: #left
+                if (g_lt_row==0 and g_lt_col==0 and g_rb_row==MAX_ROW-1 and g_rb_col==MAX_COL-1):
+                    g_MouseClickList.append(event.pos)
+                    if len(g_MouseClickList) > 2:
+                        g_MouseClickList.pop(0)
+            elif event.button == 2:
+                g_MouseClickList = []
+                g_lt_row = 0
+                g_lt_col = 0
+                g_rb_row = MAX_ROW - 1
+                g_rb_col = MAX_COL - 1
+            elif event.button == 3: #right
+                if len(g_MouseClickList) == 2:
+                    sta_pos,end_pos = g_MouseClickList
+                    g_MouseClickList = []                    
+                    if sta_pos[0]<end_pos[0] and sta_pos[1]<end_pos[1]:
+                        g_lt_col = trans_screen_x(sta_pos[0])*MAX_COL/SCREEN_WIDTH
+                        g_lt_row = trans_screen_z(sta_pos[1])*MAX_ROW/SCREEN_HEIGHT
+                        g_rb_col = trans_screen_x(end_pos[0])*MAX_COL/SCREEN_WIDTH
+                        g_rb_row = trans_screen_z(end_pos[1])*MAX_ROW/SCREEN_HEIGHT
+    screen.fill(COLOR_WHITE)
+    # print("draw blk start---")
+    # print "rc is: ",g_lt_col,g_lt_row,g_rb_col,g_rb_row
+    min_r = g_lt_row*BLK_MAX_ROW/MAX_ROW
+    max_r = g_rb_row*BLK_MAX_ROW/MAX_ROW
+    min_c = g_lt_col*BLK_MAX_COL/MAX_COL
+    max_c = g_rb_col*BLK_MAX_COL/MAX_COL
+    # print "range r ",min_r,max_r,"range c ",min_c,max_c
+    dr = max_r - min_r + 1
+    dc = max_c - min_c + 1
+    # print "dr ",dr,"dc ",dc
+    #debug line
+    # pygame.draw.line(screen, COLOR_BLUE, (min_c*SCREEN_WIDTH/BLK_MAX_COL, min_r*SCREEN_HEIGHT/BLK_MAX_ROW), (max_c*SCREEN_WIDTH/BLK_MAX_COL, max_r*SCREEN_HEIGHT/BLK_MAX_ROW))
+    for r,clst in BlkDict.items():
+        if r < min_r or r > max_r:
+            continue
+        h1 = (r-min_r)*SCREEN_HEIGHT/dr
+        h2 = (r-min_r+1)*SCREEN_HEIGHT/dr
+        for t in clst:
+            if t[1] < min_c:
+                continue
+            if t[0] > max_c:
+                break
+            s = max(t[0],min_c)
+            e = min(t[1],max_c)
+            w1 = (s-min_c)*SCREEN_WIDTH/dc
+            w2 = (e-min_c)*SCREEN_WIDTH/dc
+            pygame.draw.rect(screen, COLOR_CHOCOLATE, (w1,h1,w2-w1,h2-h1+1))
 
-    screen.fill(COLOR_CHOCOLATE)
-    if not MAP_INIT:
-        time.sleep(1)
-        continue
+    # print("draw blk end---")
 
-    print("draw blk start---")
-    # for r,clst in BlkDict.items():
-    #     for t in clst:
-    #         print "r",r,"t",t
-    #         h1 = SCREEN_HEIGHT*r/BLK_MAX_ROW
-    #         w1 = SCREEN_WIDTH*t[0]/BLK_MAX_COL
-    #         h2 = SCREEN_HEIGHT*(r+1)/BLK_MAX_ROW
-    #         w2 = SCREEN_WIDTH*(t[1]+1)/BLK_MAX_COL
-    #         print "draw",(w1,h1,w2,h2)
-    #         pygame.draw.rect(screen, COLOR_WHITE, (w1,h1,w2,h2))
-    for r,d in BlkDict.items():
-        for c,v in d.items():
-            h1 = SCREEN_HEIGHT*r/BLK_MAX_ROW
-            w1 = SCREEN_WIDTH*c/BLK_MAX_COL
-            h2 = SCREEN_HEIGHT*(r+1)/BLK_MAX_ROW
-            w2 = SCREEN_WIDTH*(c+1)/BLK_MAX_COL
-            if v == 0:
-                pygame.draw.rect(screen, COLOR_WHITE, (w1,h1,w2,h2))
-            else:
-                pygame.draw.rect(screen, COLOR_CHOCOLATE, (w1,h1,w2,h2))
-
-    print("draw blk end---")
-
-    # for i in xrange(1,MAX_ROW):
-    #     height = SCREEN_HEIGHT*i/MAX_ROW
-    #     pygame.draw.line(screen, COLOR_BLACK, (0, height), (SCREEN_WIDTH, height))
-    # for j in xrange(1,MAX_COL):
-    #     width = SCREEN_WIDTH*j/MAX_COL
-    #     pygame.draw.line(screen, COLOR_BLACK, (width, 0), (width, SCREEN_HEIGHT))
+    for i in xrange(1,MAX_ROW):
+        if i < g_lt_row:
+            continue
+        if i > g_rb_row:
+            break
+        height = SCREEN_HEIGHT*(i-g_lt_row)/(g_rb_row-g_lt_row+1)
+        pygame.draw.line(screen, COLOR_BLACK, (0, height), (SCREEN_WIDTH, height))
+        text = my_font.render(str(i+1), True, COLOR_BLUE)
+        screen.blit(text, (0, height))
+    for j in xrange(1,MAX_COL):
+        if j < g_lt_col:
+            continue
+        if j > g_rb_col:
+            continue
+        width = SCREEN_WIDTH*(j-g_lt_col)/(g_rb_col-g_lt_col+1)
+        pygame.draw.line(screen, COLOR_BLACK, (width, 0), (width, SCREEN_HEIGHT))
+        text = my_font.render(str(j+1), True, COLOR_BLUE)
+        screen.blit(text, (width, SCREEN_HEIGHT*(g_rb_col-g_lt_col)/(g_rb_col-g_lt_col+1)))
 
     # for i in xrange(0,MAX_ROW):
     #     text = my_font.render(str(i+1), True, COLOR_BLUE)
@@ -250,8 +303,19 @@ while True:
     #     text = my_font.render(str(j+1), True, COLOR_BLUE)
     #     screen.blit(text, (SCREEN_WIDTH*(j)/MAX_COL, SCREEN_HEIGHT*(MAX_ROW-1)/MAX_ROW))
 
-    # for id,obj in ObjectList.items():
-    #     obj.draw(screen)
+    if g_MouseClickList:
+        sta_pos = g_MouseClickList[0]
+        ltc = trans_screen_x(sta_pos[0])*MAX_COL/SCREEN_WIDTH
+        ltr = trans_screen_z(sta_pos[1])*MAX_ROW/SCREEN_HEIGHT
+        pygame.draw.rect(screen, COLOR_BLUE, (ltc*SCREEN_WIDTH/MAX_COL,ltr*SCREEN_HEIGHT/MAX_ROW,SCREEN_WIDTH/MAX_COL,SCREEN_HEIGHT/MAX_ROW), 3)
+        if len(g_MouseClickList) > 1:
+            end_pos = g_MouseClickList[1]
+            rbc = trans_screen_x(end_pos[0])*MAX_COL/SCREEN_WIDTH
+            rbr = trans_screen_z(end_pos[1])*MAX_ROW/SCREEN_HEIGHT
+            pygame.draw.rect(screen, COLOR_RED, (rbc*SCREEN_WIDTH/MAX_COL,rbr*SCREEN_HEIGHT/MAX_ROW,SCREEN_WIDTH/MAX_COL,SCREEN_HEIGHT/MAX_ROW), 3)
+
+    for id,obj in ObjectList.items():
+        obj.draw(screen)
 
     pygame.display.update()
-    # time.sleep(0.1)
+    g_Clock.tick(10)
